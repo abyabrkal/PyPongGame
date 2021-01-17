ddc2d37929cad3d463630df0fc92124ccb04f0f8
Aby Kal <abyabrkal@gmail.com>
2021-01-17 21:35:40 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import Screen
+
+## 1.Create a screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.title("The  Pong Game")
+#screen.tracer(0)
+
+## 2.Create and move paddle
+## 3.Create another paddle
+## 4.Create a ball and move
+## 5.Detect collision with wall and bounce
+## 6.Detect collision with paddle
+## 7.Detect when paddle misses
+## 8.Keep score
+
+
+print('Pong Game!')
+screen.exitonclick()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
