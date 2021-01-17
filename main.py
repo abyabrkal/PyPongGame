d03de3a28a2f7feff194424e8c1acf126ab05ec1
Aby Kal <abyabrkal@gmail.com>
2021-01-17 21:43:10 +0400

    Create Paddle - part1

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 ## 1.Create a screen
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
-screen.title("The  Pong Game")
+screen.title("The Pong Game")
 #screen.tracer(0)
 
 ## 2.Create and move paddle
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+
 ## 3.Create another paddle
 ## 4.Create a ball and move
 ## 5.Detect collision with wall and bounce
